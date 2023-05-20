08bc601d486eefb88ba26d907e61575f782f67c4
Minafuh <amina3647@gmail.com>
2023-05-20 03:38:02 -0700

    5th try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-
-int main (void) {
-	char str1[15];
-
-	strcpy(str1, "Programming is like building a multilingual puzzle");
-
-	puts(str1);
+/**
+ * main - prints a line with puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return(0);
 }
