4d0671282e766af6d1376139e665873f98724b60
Minafuh <amina3647@gmail.com>
2023-05-20 03:22:10 -0700

    put func4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
+int main (void) {
 	char str1[15];
 
 	strcpy(str1, "Programming is like building a multilingual puzzle");
 
-	put(str1);
+	puts(str1);
 
 	return(0);
 }
