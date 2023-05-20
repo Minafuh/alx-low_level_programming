7f03ab5f4546122e439429ae20f7c4e2af010fb3
Minafuh <amina3647@gmail.com>
2023-05-20 03:39:43 -0700

    another try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - prints a line with puts
  * Return: 0 (success)
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
