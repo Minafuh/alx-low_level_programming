482f3be665713dc4c22a4255ee8d5e72f4c04f83
Minafuh <amina3647@gmail.com>
2023-05-20 03:08:52 -0700

    put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+int main () {
+	char str1[15];
+
+	strcpy(str1, "Programming is like building a multilingual puzzle");
+
+	put(str1);
+
+	return(0);
+}
