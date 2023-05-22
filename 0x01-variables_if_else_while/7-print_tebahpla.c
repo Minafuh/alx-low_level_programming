f6a005ad7f83ceecacad26295768523de5646667
Minafuh <amina3647@gmail.com>
2023-05-22 07:24:43 -0700

    alph

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-        char a;
+	char a;
+	
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar (a);
+	}
+	putchar ('\n');
 
-        for (a = 'z'; a >= 'a'; a--)
-        {
-                putchar (a);
-        }
-        putchar ('\n');
-
-        return (0);
+	return (0);
 }
