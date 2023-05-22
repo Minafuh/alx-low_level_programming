347de605aea34da89e652d1edbfc61e577cc2aa6
Minafuh <amina3647@gmail.com>
2023-05-22 06:00:44 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints the alphabet in lowercase
+ * followed by a new line
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	return(0);
+}
