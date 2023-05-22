d4c18217bcc20d7263fe0501ed44231b4b849f1c
Minafuh <amina3647@gmail.com>
2023-05-22 07:14:54 -0700

    num

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 /**
  * main - a program that prints all single digit numbers of base 10
  *
- * Return: 0
+ * Return: 0 (success)
  */
 int main(void)
 {
-	short unsigned int a;
+	int a;
 
-	for (a = 0; a < 10; a++)
+	for (a = '0'; a < '10'; a++)
 	{
 		putchar (a);
 	}
