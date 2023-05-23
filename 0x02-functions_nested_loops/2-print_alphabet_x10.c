d19d22d61320e5d7a9dd6a4e2a9e974bbf4d5e50
Minafuh <amina3647@gmail.com>
2023-05-23 04:11:10 -0700

    ten time alphab

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  a function that prints the alphabet 10 times, in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+        char a;
+
+        for (a = 'a'; a <= 'z'; a++)
+        {
+                _putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+		_putchar(a);
+        }
+        _putchar('\n');
+}
