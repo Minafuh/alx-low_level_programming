de16e9d314aec2c5f59cc41628150cc150b4b010
Minafuh <amina3647@gmail.com>
2023-05-23 04:15:12 -0700

    ten time alphab

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
         for (a = 'a'; a <= 'z'; a++)
         {
-                _putchar(aaaaaaaaaa);
+                _putchar(a)*10;
         }
         _putchar('\n');
 }
