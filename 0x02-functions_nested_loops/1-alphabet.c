a194d12bd4321804b4fd8e08469a3c1f7e548c8e
Minafuh <amina3647@gmail.com>
2023-05-23 03:59:30 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -  a function that prints the alphabet, in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char a;
+	for (a = a, a <= z; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	
+	return (0);
+}
