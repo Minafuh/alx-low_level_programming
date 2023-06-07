a120a8f5120d4d175b399b7a3e8c72b1abebad27
Minafuh <amina3647@gmail.com>
2023-06-07 03:55:17 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s : the string to be measured
+ * Return the length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int ln = 0;
+
+	if (*s)
+	{
+		ln++;
+		ln += _strlen_recursion(s + 1);
+	}
+	return(ln);
+}
+
