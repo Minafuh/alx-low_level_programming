2da1f928457a83dec2a43492dcc33b538c3aa772
Minafuh <amina3647@gmail.com>
2023-06-15 04:37:38 -0700

    more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc and exit if fail
+ *@b: int
+ * Return: pointer to the array intialized or NULL
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+
+		exit(98);
+
+	return (m);
+}
